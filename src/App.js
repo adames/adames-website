391d752bf563a71042b25d40b7a94b0a8d2ab56d
adames <adames.hodelin@gmail.com>
2023-06-28 20:39:45 -0400

    adding does it apply?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Messing with the html
         </p>
         <a
           className="App-link"
