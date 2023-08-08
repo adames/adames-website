8ad1e14a43d9a80a6b3272ae6caf3139c55e1fee
adames <adames.hodelin@gmail.com>
2023-08-07 20:37:39 -0600

    adding main page texts

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,18 @@
 const Home = () => {
     return (
-        <div>Hello Home</div>
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover your emotions
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    AI-Powered emotions
+                </span>
+            </h1>
+            <p className="desc text-center">
+                This is an open-source Ai tool for sentiment analysis in journaling.
+            </p>
+            {/* Feed */}
+        </section>
     )
 }
 export default Home;
