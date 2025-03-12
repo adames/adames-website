b25a6621151aa5d66e48ce0415ef1562273964a2
adames <adames.hodelin@gmail.com>
2025-03-12 10:16:43 -0400

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>adames.cc</h1>
+      <p>This will be the home page of my website</p>
     </div>
   );
 };
