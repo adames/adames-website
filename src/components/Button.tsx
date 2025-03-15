dc733f5c72c70838d5a69a6aeaaffbbf2c68fe7c
adames <adames.hodelin@gmail.com>
2025-03-15 17:01:17 -0400

    buttons

---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+const Button = () => {
+  return (
+    <button className="btn">
+      Click Me
+    </button>
+  );
+}
+export default Button;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,13 @@
+import Button from "./Button";
+
+
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">MyApp</div>
-        <ul className="flex space-x-4">
-          <li>
-            <a href="#" className="text-gray-300 hover:text-white">Home</a>
-          </li>
-          <li>
-            <a href="#" className="text-gray-300 hover:text-white">About</a>
-          </li>
-          <li>
-            <a href="#" className="text-gray-300 hover:text-white">Contact</a>
-          </li>
-        </ul>
+    <nav className="navbar">
+      <div className="navbar-content">
+        <Button />
+        <Button />
+        <Button />
       </div>
     </nav>
   );
